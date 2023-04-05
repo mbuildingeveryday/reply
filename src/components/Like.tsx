7d83a9c34848b356fb2af010eba1da7343b592a5
Minki Kim <mbuildingeveryday@gmail.com>
2023-04-05 22:11:07 +0900

    Like component 추가

---
+++ src/components/Like.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { LikeProps } from "../types/types";
+
+function Like(props: LikeProps) {
+    const like = props.like;
+
+    return (
+        <div>
+            {like ? <img className="like-img" src={'images/favorite-icon.png'} /> : <></>}
+        </div>
+    )
+}
+
+export default Like;
