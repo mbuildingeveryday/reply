87ba3ead02c28de5395d356d85f7efc482396d43
Minki Kim <mbuildingeveryday@gmail.com>
2023-04-06 19:20:43 +0900

    말풍선 open button 추가 및 표시 위치 설정

--- src/components/CommentListItem.tsx
+++ src/components/CommentListItem.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppDispatch } from "../hooks/hooks";
 import { onClickLikeBtn, onClickReplyBtn } from "../redux/slices/commentSlice";
 import { CommentListItemProps } from "../types/types";
 import CommentInputLine from "./CommentInputLine";
+import ModalOption from "./ModalOption";
 import Like from "./Like";
 
 function CommentListItem(props: CommentListItemProps) {
     const dispatch = useAppDispatch();
     const className = props.className;
     const index = props.index;
+    const commentIndex = props.commentIndex;
     const comment = props.comment;
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [displayOption, setDisplayOption] = useState(false);
+
+    const handleOption = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setPosition({ x: e.currentTarget.getBoundingClientRect().x - 40, y: e.currentTarget.getBoundingClientRect().y + 20 });
+        setDisplayOption(!displayOption);
+    }
 
     const renderReplies = () => {
         return (
             <ul className={"comment-list"}>
-                {comment.reply.map((value, replyIndex) => <CommentListItem key={replyIndex + value.comment.slice(0, 5)} index={replyIndex} className={`comment-list__li`} comment={value} />)}
+                {comment.reply.map((value, replyIndex) => <CommentListItem key={replyIndex + value.comment.slice(0, 5)} index={replyIndex} commentIndex={index} className={`comment-list__li`} comment={value} />)}
             </ul>
         )
     }
@@ -27,8 +36,12 @@ function CommentListItem(props: CommentListItemProps) {
 
     return (
         <li className={`${className}${comment.isReply ? "--reply" : ""}`}>
+            <ModalOption index={index} commentIndex={commentIndex} position={position} display={displayOption} setDisplay={setDisplayOption} comment={comment} />
             <div className={`${className}-main`}>
-                <p className={`${className}-main-title`}>{comment.name}</p>
+                <div className={`${className}-main-title-container`}>
+                    <span className={`${className}-main-title`}>{comment.name}</span>
+                    <button onClick={(e) => { handleOption(e) }} className={`${className}-edit-btn`}>···</button>
+                </div>
                 <p className={`${className}-main-des`}>{comment.comment}</p>
                 {!comment.isReply ?
                     <div className={`${className}-main-btns`}>
